57084ad87a0626ce6620b06e50adaaa9a178b22e
cristofersousa <cristofer.sousa@ielusc.br>
2019-11-11 19:59:28 -0300

    create model pastel

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
+const bodyParser = require('body-parser');
+const requireDir = require('require-dir');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+requireDir('./src/models');
+const Pastel = mongoose.model('Pastel');
 
 app.get('/', (req, res) => {
   res.send({ message: 'hello pastel'});
